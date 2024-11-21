b200e110e8142d7c47d03aa78d39c3524439e4ce
Vladimir <kazimirov.2013@mail.ru>
2024-11-21 20:08:58 +0300

    Add: Добавил класс TaskService, прописал базовые методы

---
+++ src/main/java/com/kazimirov/financemanagement/service/TaskService.java
@@ -0,0 +1,44 @@
+package com.kazimirov.financemanagement.service;
+
+import com.kazimirov.financemanagement.model.Task;
+import com.kazimirov.financemanagement.model.TaskStatus;
+import com.kazimirov.financemanagement.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getTaskByTitle(String title) {
+        return taskRepository.findByTitleContaining(title);
+    }
+
+    public List<Task> getTaskByStatus(TaskStatus status) {
+        return taskRepository.findByStatus(status);
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public void deleteTask(Long id) {
+        if (!taskRepository.existsById(id)) {
+            throw new IllegalArgumentException("Задача с ID " + id + " не найдена.");
+        }
+        taskRepository.deleteById(id);
+    }
+}
