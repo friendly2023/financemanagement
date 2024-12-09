a80f5646f4ba09676f9e7ee6ddf035c4be71fb00
Vladimir <kazimirov.2013@mail.ru>
2024-12-09 23:20:26 +0300

    Add: клиенты удаляются

--- src/main/java/com/kazimirov/financemanagement/controller/ClientController.java
+++ src/main/java/com/kazimirov/financemanagement/controller/ClientController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -37,5 +38,11 @@ public class ClientController {
     public String createClient(ClientEntity clientEntity) {
         clientService.createClient(clientEntity);
         return "redirect:/";
+    }
+
+    @PostMapping("/clients/delete/{id}")
+    public String deleteClient(@PathVariable Long id) {
+        clientService.deleteClient(id);
+        return "redirect:/clients";
     }
 }
